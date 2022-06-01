9fc7281188d1efc71e4bd189003eed8889c65891
Inma Gijón <igijoninf@gmail.com>
2022-06-01 18:51:45 +0200

    Filtrado de respuesta. Optimización peticiónes HTTP

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 import { Country } from '../interfaces/pais.interface';
 
@@ -12,19 +12,23 @@ export class PaisService {
 
   private _apiUrl: string = 'https://restcountries.com/v3.1';
 
+  get httpParams() {
+    return new HttpParams()
+    .set('fields', 'name,capital,cca2,population,flags');
+  }
   constructor(private http: HttpClient) { }
 
   buscarPais( termino: string ): Observable<Country[]> {
 
     const url = `${ this._apiUrl }/name/${ termino }`;
-    return this.http.get<Country[]>( url );
+    return this.http.get<Country[]>( url, {params: this.httpParams} );
 
   }
 
   buscarCapital( termino: string ): Observable<Country[]> {
 
     const url = `${ this._apiUrl }/capital/${ termino }`;
-    return this.http.get<Country[]>( url );
+    return this.http.get<Country[]>( url, {params: this.httpParams} );
 
   }
 
@@ -36,9 +40,11 @@ export class PaisService {
   }
 
   buscarRegion( region: string ): Observable<Country[]> {
-
     const url = `${ this._apiUrl }/region/${ region }`;
-    return this.http.get<Country[]>( url );
+    return this.http.get<Country[]>( url, { params: this.httpParams} )
+      .pipe(
+        tap(console.log)
+      );
 
   }
 }
