33a3761a75cd48a96121b843bfb9a0d060784ac3
Inma Gijón <igijoninf@gmail.com>
2022-06-01 10:08:48 +0200

    Tabla con datos de países

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -11,6 +12,8 @@ export class PorPaisComponent {
 
   termino: string = '';
   hayError: boolean = false;
+  paises: Country[] = [];
+
   constructor(private paisService: PaisService) { }
 
   buscar() {
@@ -18,10 +21,11 @@ export class PorPaisComponent {
     this.paisService.buscarPais( this.termino )
       .subscribe( {
         next: (paises) => {
-          console.log(paises);
+          this.paises = paises;
         },
         error: (err) => {
           this.hayError = true;
+          this.paises = [];
         }
       });
   }
