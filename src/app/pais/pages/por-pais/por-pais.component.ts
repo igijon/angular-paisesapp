4bde1e34a8d834004cd2146de73782088be56ca9
Inma Gijón <igijoninf@gmail.com>
2022-06-02 15:38:37 +0200

    Sugerencias al escribir.

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,6 +6,11 @@ import { PaisService } from '../../services/pais.service';
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `
   ]
 })
 export class PorPaisComponent {
@@ -13,12 +18,15 @@ export class PorPaisComponent {
   termino: string = '';
   hayError: boolean = false;
   paises: Country[] = [];
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias: boolean = false;
 
   constructor(private paisService: PaisService) { }
 
   buscar(termino: string) {
     this.termino = termino;
     this.hayError = false;
+    this.mostrarSugerencias = false;
     this.paisService.buscarPais( this.termino )
       .subscribe( {
         next: (paises) => {
@@ -33,7 +41,17 @@ export class PorPaisComponent {
 
   sugerencias( termino: string ) {
     this.hayError = false;
-    // TODO: Crear sugerencias
-  }
+    this.termino = termino;
+    this.mostrarSugerencias = true;
 
+    this.paisService.buscarPais( termino )
+      .subscribe({
+        next: (paises) => {
+          this.paisesSugeridos = paises.splice(0, 3);
+        },
+        error: (err) => {
+          this.paisesSugeridos = [];
+        }
+    });
+  }
 }
